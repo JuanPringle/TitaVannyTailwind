f25e68f58b56e38a2b4978be7f71a261a4b8356c
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2023-12-12 16:58:39 -0500

    Fix clipping on the jobs

--- my-project/src/Pages/JobSearch.jsx
+++ my-project/src/Pages/JobSearch.jsx
@@ -3,7 +3,7 @@ import Job from "../Components/Job";
 function JobSearch(){
     return (  
         <>
-        <div className='p-12 grid grid-cols-4 gap-x-1 gap-y-1 min-h-screen block bg-pasadena bg-no-repeat bg-fixed bg-cover flex items-center backdrop-invert'>
+        <div className='p-12 pt-32 grid-cols-4 gap-x-1 gap-y-1 min-h-screen block bg-pasadena bg-no-repeat bg-fixed bg-cover flex items-center'>
             
             
             <Job></Job>
