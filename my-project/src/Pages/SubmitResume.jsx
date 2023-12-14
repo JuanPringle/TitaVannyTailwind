4ca387b27125f9e7cac523664dd1dde6ecee5425
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-12-13 21:11:57 -0500

    Update SubmitResume.jsx

--- my-project/src/Pages/SubmitResume.jsx
+++ my-project/src/Pages/SubmitResume.jsx
@@ -8,23 +8,23 @@ function SubmitResume() {
       
     </div>
     <div className="border-2 border-double rounded border-slate-100 py-12 px-5 bg-slate-800 opacity-90">
-    <label className="text-white block text-center">
+      <label className="text-white block text-left">
         Name
       </label>
-      <input type="text" name="file"  className="pr-20 text-black hover:bg-slate-300 text-center rounded" />
-      <label className="pt-5 text-white block text-center">
+      <input type="text" name="file"  className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <label className="pt-5 text-white block text-left">
         Phone
       </label>
-      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-center rounded" />
-      <label className="pt-5 text-white block text-center">
+      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <label className="pt-5 text-white block text-left">
         Email
       </label>
-      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-center rounded" />
-      <label className="pt-5 text-white block text-center">
+      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <label className="pt-5 text-white block text-left">
         Comments
       </label>
-      <textarea name="body" id="" cols="30" rows="4"></textarea>
-      <label className="pt-5 text-white block text-center">
+      <textarea type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" id="" cols="22" rows="4"></textarea>
+      <label className="pt-5 text-white block text-left">
         Drop Resume here
       </label>
       <input type="file" name="file" className="text-white text-center rounded" />
