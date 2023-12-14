bf8989bc160b70b8d82de86e0ea974120c2b64a3
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-12-13 21:22:31 -0500

    started contact

--- my-project/src/Pages/Contact.jsx
+++ my-project/src/Pages/Contact.jsx
@@ -1,11 +1,38 @@
 const Contact = () => {
   return (
-    <div className="flex align-middle min-h-screen items-center">
-      <label className=" text-white">
-        Select Files
-        <input type="file"name="file" ></input>
-      </label>
-    </div>
+    <>
+      <div className='min-h-screen bg-callUs bg-no-repeat bg-fixed bg-cover flex items-center'>
+        <div>
+          <p className='text-white text-center text-5xl font-body block mb-2 ml-32 bg-black p-3 bg-opacity-80'>CONTACT US</p>
+          <p className='text-white text-center block text-xl font-body ml-32 bg-black p-2 bg-opacity-80'>Scroll Down</p>
+        </div>
+      </div>
+      <div className="mx-10">
+        <p className="text-blue-500 text-4xl font-body pb-3 pt-6">
+          WHO WE ARE
+        </p>
+        <p className=" text-white pb-3 text-2xl">
+          Career Advantage is a full-service staffing agency that provides comprehensive
+          staffing solutions in Los Angeles and the surrounding areas.  We began in 2001 as a
+          two person search firm and have been on a journey of growth since.  After taking stock
+          of who we are and where we've been, one thing became clear; maintaining the status quo
+          was no way to run a business. We realized to keep growing, we'd have to be infinitely
+          more invested. In our own people, clients, and candidates. In everything we do, every
+          single day. So we made a commitment to do exactly that. And we've remained true to it ever since.
+        </p>
+
+
+        <p className=" text-white py-3 pt-10 text-2xl">
+          No matter what position you're looking to fill, you can be confident every candidate we
+          present will be more qualified, more skilled and more of what you are looking for. If you're
+          looking for work, you can be confident that our clients have highly desirable positions that
+          you may not find using typical job search methods.  And that we have the relationships and
+          connections to expose you to the industries and positions you want.
+        </p>
+
+        
+      </div>
+    </>
   )
 }
 
