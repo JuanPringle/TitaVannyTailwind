0a8a41cb735f4d4bfe671a36a0d5b793627b874a
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-12-13 23:32:04 -0500

    trying to add google maps

--- my-project/src/Pages/Contact.jsx
+++ my-project/src/Pages/Contact.jsx
@@ -7,12 +7,12 @@ const Contact = () => {
           <p className='text-white text-center block text-xl font-body ml-32 bg-black p-2 bg-opacity-80'>Scroll Down</p>
         </div>
       </div>
-      <div className="my-10 mx-10 sm:columns-1 md:columns-2">
+      <div className="my-10 mx-10 sm:mx-10 md:mx-32 lg:mx-52 sm:columns-1 md:columns-1 lg:columns-2">
         <div>
           <p className="text-blue-500 text-4xl font-body pb-3 pt-6 ">
             CAREER ADVANTAGE, INC.
           </p>
-          <p className="text-white text-xl font-body pb-3 ">
+          <p className="text-white text-2xl font-body pb-3 ">
             <div>
               <h4>3460 Wilshire Blvd.,</h4>
               <h4>Suite 1126</h4>
@@ -20,22 +20,22 @@ const Contact = () => {
             </div>
                 
           </p>
-          <p className="text-blue-500 text-3xl font-body pt-6">
+          <p className="text-blue-500 text-3xl font-body pt-5">
             PHONE
           </p>
-          <p className="text-white text-xl font-body pb-3 ">
+          <p className="text-white text-2xl font-body pb-3 ">
             (213) 380-4410
           </p>
           <p className="text-blue-500 text-3xl font-body  pt-5">
             FAX
           </p>
-          <p className="text-white text-xl font-body pb-3 ">
+          <p className="text-white text-2xl font-body pb-3 ">
             (213) 380-4421
           </p>
           <p className="text-blue-500 text-3xl font-body  pt-5">
             EMAIL
           </p>
-          <p className="text-white text-xl font-body pb-3 ">
+          <p className="text-white text-2xl font-body pb-3 ">
             hr@caistaffing.com
           </p>
           <p className="text-blue-500 text-3xl font-body  pt-5">
@@ -43,7 +43,7 @@ const Contact = () => {
           </p>
           
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-            <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif"/>
+            <input className="pb-6" type="image" width="200" src="https://www.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif"/>
           </form>
         </div>
         <div>
@@ -54,33 +54,48 @@ const Contact = () => {
             <p className="text-white text-lg font-bold pb-3">
               All fields required unless noted
             </p>
+              
             <label className="pb-1 text-white block text-left">
               Name
             </label>
-            <input type="text" name="file"  className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+            <input type="text" name="NAME"  id="edit_15" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
             <label className="pt-5 pb-1 text-white block text-left">
               Phone Number (optional)
             </label>
-            <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+            <input  type="text" name="Phone" id="edit_14" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
             <label className="pt-5 pb-1 text-white block text-left">
               Email
             </label>
-            <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+            <input type="text" name="EMAIL" id="edit_2" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
             <label className="pt-5 pb-1 text-white block text-left">
               Message
             </label>
-            <textarea type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" id="" cols="22" rows="4"></textarea>
-            <button className="text-left border-black mt-5 bg-slate-100 hover:bg-slate-400 text-slate-700 hover:text-black rounded my-9 p-1 px-2 font-bold">Submit </button>
+            <textarea type="text" name="COMMENT" id="text_1" className="pr-20 text-black hover:bg-slate-300 text-left rounded"  cols="22" rows="4"></textarea>
+            <button className="block text-left border-black mt-5 bg-slate-100 hover:bg-slate-400 text-slate-700 hover:text-black rounded my-9 p-1 px-2 font-bold">Submit </button>
+            
           </div>
-        </div>        
+        </div>     
         
-
+        
+        
         
       </div>
       <div>
         <p className="text-blue-500 text-3xl font-body pb-3 pt-6 text-center">
             GET DIRECTION
         </p>
+        {null &&
+          <div className="google-maps">
+          <iframe
+            src=""
+            width="600"
+            height="  450"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+          </div>   
+        }
       </div>
     </>
   )
--- my-project/src/Pages/SubmitResume.jsx
+++ my-project/src/Pages/SubmitResume.jsx
@@ -11,24 +11,24 @@ function SubmitResume() {
       <label className="text-white block text-left">
         Name
       </label>
-      <input type="text" name="file"  className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <input type="text" name="NAME" id="edit_1" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
       <label className="pt-5 text-white block text-left">
         Phone Number
       </label>
-      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <input type="text" name="Phone" id="edit_2" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
       <label className="pt-5 text-white block text-left">
         Email
       </label>
-      <input type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
+      <input type="text" name="Email" id="edit_3" className="pr-20 text-black hover:bg-slate-300 text-left rounded" />
       <label className="pt-5 text-white block text-left">
         Comments
       </label>
-      <textarea type="text" name="file" className="pr-20 text-black hover:bg-slate-300 text-left rounded" id="" cols="22" rows="4"></textarea>
+      <textarea type="text" name="Comments" id="text_2" className="pr-20 text-black hover:bg-slate-300 text-left rounded"  cols="22" rows="4"></textarea>
       <label className="pt-5 text-white block text-left">
         Drop Resume here
       </label>
-      <input type="file" name="file" className="text-white text-center rounded" />
-      <button className="text-left border-black mt-5 bg-slate-100 hover:bg-slate-400 text-slate-700 hover:text-black rounded my-9 p-1 px-2 font-bold">Submit </button>
+      <input type="file" name="UPLOAD" id="file_1" className="text-white text-center rounded" />
+      <button className="block text-left border-black mt-5 bg-slate-100 hover:bg-slate-400 text-slate-700 hover:text-black rounded my-2 p-1 px-4 font-bold">Submit </button>
     </div>
   </div>
 </>
